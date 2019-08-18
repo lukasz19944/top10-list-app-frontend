34b53809e5e8f665a624858eea69809b99f2d0ed
lukasz19944 <lukasz19944@gmail.com>
2019-08-18 12:09:59 +0200

    Redux store. Closes #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <p>Test</p>
+      </div>
+    </Provider>
   );
 }
 
